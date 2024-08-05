00e4d26bfc3ccc10af835821fe95c7b6773ec239
BaptisteFredj <redj2304@gmail.com>
2024-08-05 16:13:41 +0200

    harmonia05 quest

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -10,3 +10,20 @@ create table item (
   user_id int unsigned not null,
   foreign key(user_id) references user(id)
 );
+
+CREATE Table category (
+  id int unsigned primary key auto_increment not null,
+  name varchar(255) not null
+);
+
+CREATE Table program (
+  id int unsigned primary key auto_increment not null,
+  title varchar(255) not null,
+  synopsis varchar(255) not null,
+  poster varchar(255) not null,
+  country varchar(255) not null,
+  year int not null,
+  category_id INT UNSIGNED NOT NULL,
+  foreign key (category_id) references category(id)
+);
+
